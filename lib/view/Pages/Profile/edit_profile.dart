bdea492408d718ba37e1f1bd4619010638f24434
jejelagibanyaktugas <jejelagibanyaktugas@gmail.com>
2024-11-08 03:36:30 +0000

    update edit profile page

--- lib/view/Pages/Profile/edit_profile.dart
+++ lib/view/Pages/Profile/edit_profile.dart
@@ -3,11 +3,50 @@ import 'package:flutter/material.dart';
 class EditProfile extends StatelessWidget {
   const EditProfile({super.key});
 
+  Widget _buildEditableIconRow(IconData icon, String text, double fontSize,
+      TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InkWell(
+            onTap: () {},
+            child: Row(
+              children: [
+                Icon(icon, color: Color(0xFFB4A9D6)),
+                SizedBox(width: 10),
+                Text(
+                  text,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Montserrat',
+                    color: Color(0xFFB4A9D6),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              border: UnderlineInputBorder(),
+              hintText: 'Enter $text',
+              hintStyle: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double titleFontSize = screenWidth * 0.06;
-    // double subtitleFontSize = screenWidth * 0.04;
+    double subtitleFontSize = screenWidth * 0.04;
 
     return Scaffold(
       // Section 1: Title and Back Button
@@ -33,7 +72,7 @@ class EditProfile extends StatelessWidget {
               title: Padding(
                 padding: const EdgeInsets.only(left: 0),
                 child: Text(
-                  'Edit Profile',
+                  'Profile',
                   style: TextStyle(
                     fontSize: titleFontSize,
                     fontWeight: FontWeight.bold,
@@ -48,7 +87,60 @@ class EditProfile extends StatelessWidget {
               pinned: false,
             ),
           ];
-        }, body: Center(),
+        },
+
+        // Section 2: Edit Profile Contents
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              // Wrap with SingleChildScrollView
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 25),
+
+                  // Profile Pic
+                  CircleAvatar(
+                    radius: 75,
+                    backgroundImage: AssetImage('assets/images/Contoh 1.png'),
+                  ),
+
+                  SizedBox(height: 15),
+
+                  // Change Picture Hint
+                  Text(
+                    'Change Profile Picutre',
+                    style: TextStyle(
+                      fontSize: subtitleFontSize,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat',
+                      color: Color(0xFFB4A9D6),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  // Name and E-mail Text Field
+                  Column(
+                    children: [
+                      _buildEditableIconRow(
+                        Icons.person,
+                        'Name',
+                        subtitleFontSize,
+                        TextEditingController(),
+                      ),
+                      _buildEditableIconRow(
+                        Icons.mail,
+                        'E-mail Address',
+                        subtitleFontSize,
+                        TextEditingController(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
