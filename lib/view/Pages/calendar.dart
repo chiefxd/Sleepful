3401ece6a203fd5c79e0bcc532fe0f5c6bcb26e2
eleanor <eleanrelen@gmail.com>
2024-11-09 17:03:12 +0700

    fix error on yaml

--- lib/view/Pages/calendar.dart
+++ lib/view/Pages/calendar.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({super.key});
 
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
